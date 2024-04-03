e976ef5e5ceefda6d2df64b67d0fa23822b4749f
morits <mor1ts@mail.ru>
2024-04-03 14:02:13 +0300

    Made a flipping animation

---
+++ app/src/main/java/ru/devsokovix/test_snaphelper/MainActivity.kt
@@ -0,0 +1,21 @@
+package ru.devsokovix.test_snaphelper
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val pageSnapHelper = PagerSnapHelper()
+        pageSnapHelper.attachToRecyclerView(recyclerView)
+        recyclerView.adapter = MyAdapter()
+    }
+}
